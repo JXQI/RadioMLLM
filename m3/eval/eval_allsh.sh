a825ad105a28f25e493aaa41c2b196380987078b
jxiaoqiang <zhuan@infervision.com>
2025-11-13 17:13:32 +0800

    增加评测

---
+++ m3/eval/eval_allsh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# export PROJECT_PATH="/home/tx-deepocean/data1/jxq/code/VLM-Radiology-Agent-Framework"
+# export MODEL_PATH="/home/tx-deepocean/data1/jxq/code/VLM-Radiology-Agent-Framework/m3/demo/utils/Llama3-VILA-M3-8B"
+# export OUTPUT_FOLDER_NAME="/home/tx-deepocean/data1/jxq/code/VLM-Radiology-Agent-Framework/m3/eval/eval_results"
+# export CONV_MODE="llama_3"
+
+export PROJECT_PATH="/home/tx-deepocean/data1/jxq/code/LLaVA-Med"
+export MODEL_PATH="/home/tx-deepocean/data1/jxq/code/LLaVA-Med/llava-med-v1.5-mistral-7b-lora-mistral_instruct-heart2-chest2_slake1_path1_rad1_v4"
+export OUTPUT_FOLDER_NAME="/home/tx-deepocean/data1/jxq/code/LLaVA-Med/eval/heart2-chest2_slake1_path1_rad1_v4"
+export CONV_MODE="mistral_instruct"
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+sh eval_pathvqa.sh $MODEL_PATH $OUTPUT_FOLDER_NAME $CONV_MODE
+# sh eval_radvqa.sh $MODEL_PATH $OUTPUT_FOLDER_NAME $CONV_MODE
+# sh eval_slakevqa.sh $MODEL_PATH $OUTPUT_FOLDER_NAME $CONV_MODE
+
---
+++ m3/eval/eval_pathvqa.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#SBATCH --job-name=pathvqa_eval
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --time=2:00:00
+#SBATCH --partition=interactive,interactive_singlenode,grizzly,polar,polar2,polar3,polar4
+#SBATCH --dependency=singleton
+
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# set common env vars
+# source set_env.sh
+
+# if [[ $# -ne 3 ]]; then
+#     print_usage
+#     exit 1
+# fi
+
+export MODEL_PATH=$1
+export OUTPUT_FOLDER_NAME=$2
+export CONV_MODE=$3
+
+# # check if env vars are set
+# : ${CONTAINER:?"CONTAINER env var is not set!"}
+# : ${DATASETS:?"DATASETS env var is not set!"}
+# : ${CODE:?"CODE env var is not set!"}
+# : ${ACCOUNT:?"ACCOUNT env var is not set!"}
+
+
+DATA_PATH="/home/tx-deepocean/data1/jxq/data/processed_m3/PATH-VQA/pathvqa_instruct_test.json"
+IMAGE_DIR="/home/tx-deepocean/data1/jxq/data/processed_m3/PATH-VQA"
+OUTPUT_ROOT="$OUTPUT_FOLDER_NAME/pathvqa"
+OUTPUT_PATH="$OUTPUT_ROOT/outputs.jsonl"
+RESULT_PATH="$OUTPUT_ROOT/results.json"
+
+
+# python -m llava.eval.model_vqa_science \
+#         --model-path $MODEL_PATH \
+#         --question-file $DATA_PATH \
+#         --image-folder $IMAGE_DIR \
+#         --answers-file $OUTPUT_PATH \
+#         --num-chunks 1 \
+#         --chunk-idx 0 \
+#         --single-pred-prompt \
+#         --generation-config "$GENERATION_CONFIG" \
+#         --conv-mode $CONV_MODE 
+
+# torchrun --nproc_per_node=4 -m llava.eval.model_vqa_science \
+#         --model-path $MODEL_PATH \
+#         --question-file $DATA_PATH \
+#         --image-folder $IMAGE_DIR \
+#         --answers-file $OUTPUT_PATH \
+#         # --single-pred-prompt \
+#         --conv-mode $CONV_MODE 
+
+python $PROJECT_PATH//m3/eval/scripts/metric_pathvqa.py \
+    --input $DATA_PATH \
+    --answers $OUTPUT_PATH \
+    --output $RESULT_PATH \
+
